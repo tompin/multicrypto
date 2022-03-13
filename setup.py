bfc810c03a6ccd712cbaef4788fd1c40578121d1
tompin <tompin@tuta.io>
2022-03-13 22:13:59 +0100

    Add support for python 3.10.

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     keywords='cryptocurrency, address, transaction',
     packages=find_packages(exclude=['htmlcov', 'tests']),
